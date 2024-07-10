c55d2c168db03604bd7aa23dd2b2fcb53f8947bc
PR7175Z <aadimpraz@gmail.com>
2024-07-10 21:47:39 +0545

    Minor changes

--- mathsexpressionvalidation/script.js
+++ mathsexpressionvalidation/script.js
@@ -6,12 +6,23 @@ document.getElementById('stackform').addEventListener('submit', function(e){
     for(let i = 0; i<arr.length;i++){
         // console.log(arr[i].charCodeAt(0))
         if(arr[i].charCodeAt(0)>=33 && arr[i].charCodeAt(0)<=47){
+            if(arr[i] == ')'){
+                // temp = i;
+                s = sign.length;
+                while(sign[s-1] != '('){
+                    s--;
+                    postfix.push(sign[s]);
+                    // console.log(sign[s]);
+                }
+                // i = temp;
+                console.log(arr[i])
+            }
             sign.push(arr[i]);
         }else{
             postfix.push(arr[i]);
         }
         // console.log(arr[i]);
     }
-    console.log(postfix)
+    console.log( "postfix: " + postfix)
     console.log(sign)
 })
