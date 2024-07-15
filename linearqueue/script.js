ff9b1d9f9cee2463fb96b80966dfe0593bd66332
PR7175Z <aadimpraz@gmail.com>
2024-07-15 10:39:13 +0545

    Queue form event

--- linearqueue/script.js
+++ linearqueue/script.js
@@ -1,5 +1,19 @@
-document.getElementById('queueForm').addEventListener('submit', (e) => {
+document.getElementById('queuesize').addEventListener('submit', (e) => {
     e.preventDefault();
-    const input = document.querySelector('input[name = "queueinput"]');
-    console.log(input.value);
+    const input = document.querySelector('input[name = "size"]');
+    const field = document.getElementById('fields');
+    field.innerHTML = '';
+    for(let i = 0; i<parseInt(input.value); i++){
+        field.appendChild(document.createElement('p'));
+    }
+})
+
+document.getElementById('queueform').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const queueinput = document.querySelector('input[name="queueinput"]')
+    const fields = document.querySelectorAll('#fields p');
+    fields.forEach(element => {
+        console.log(element); 
+    });
+
 })
