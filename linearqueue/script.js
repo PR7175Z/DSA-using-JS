4a77065de71991fed70e96f28cac571283cc0436
PR7175Z <aadimpraz@gmail.com>
2024-07-17 23:29:14 +0545

    Queue methods

--- linearqueue/script.js
+++ linearqueue/script.js
@@ -6,14 +6,35 @@ document.getElementById('queuesize').addEventListener('submit', (e) => {
     for(let i = 0; i<parseInt(input.value); i++){
         field.appendChild(document.createElement('p'));
     }
+    document.getElementById('queueform').style.display = "block";
+    document.querySelector('.result').style.display = "block";
 })
 
 document.getElementById('queueform').addEventListener('submit', (e) =>{
     e.preventDefault();
-    const queueinput = document.querySelector('input[name="queueinput"]')
+    const result = document.querySelector('.result p');
+    const operation = document.querySelectorAll('input[name="queueoperation"]');
+    const queueinput = document.querySelector('input[name="queueinput"]').value;
     const fields = document.querySelectorAll('#fields p');
-    fields.forEach(element => {
-        console.log(element); 
-    });
+    for(let i=0; i<operation.length; i++){
+        if(operation[i].checked){
+            if(operation[i].value === 'push'){
+                for(let i = 0; i<fields.length; i++){
+                    if(fields[i].innerHTML == ''){
+                        fields[i].innerHTML = queueinput;
+                        break;
+                    }
+                }
+            }else{
+                for(let i = 0; i<fields.length; i++){
+                    if(fields[i].innerHTML !== ''){
+                        result.append( ' ' + fields[i].innerHTML);
+                        fields[i].innerHTML = '';
+                        break;
+                    }
+                }
+            }
+        }
+    }
 
 })
